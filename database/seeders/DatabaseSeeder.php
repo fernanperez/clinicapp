6bfcb0103d06a3f0762ec257a1dd4844bd5d571f
fernan perez <ferzao200511@hotmail.com>
2021-09-30 18:48:44 -0500

    se probaron la funcion de los seeder de empresa, ciudad, departamento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call([
+            // DepartamentosSeeder::class,
+            // CiudadesSeeder::class,
+            // EmpresasSeeder::class,
+        ]);
     }
 }
